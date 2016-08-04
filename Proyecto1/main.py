06e3277f5ed09594fea1682983df1d82dcc3e55b
Norman94 <ingnbh@gmail.com>
2016-08-04 17:34:51 -0600

    Proyecto
    Proyecto de U

---
+++ Proyecto1/main.py
@@ -0,0 +1,41 @@
+
+MAX = 5
+
+from random import randint
+
+
+def main(turnos):
+    print ("TURNOS POR PARTIDA: "+ str(turnos))
+    intentos = 1
+    flag = False
+    num = randint(10, 101)
+    print(num)
+    while (flag == False):
+        print("Intento numero:" + str(intentos))
+        numUsuario = int(input("Digite un numero:"))
+        if (num == numUsuario):
+            if (intentos == 1):
+                print("FELICIDADES, HAS LOGRADO ADIVINAR A LA PRIMERA")
+            else:
+                print("Ganaste en el: " + str(intentos) + " intento")
+            flag = True
+        elif (numUsuario < num):
+            print("Debe de digitar un numero mayor")
+        elif (num < numUsuario):
+            print("Debe de digitar un numero menor")
+
+        if (intentos == MAX):
+            flag = True
+            print("Perdiste, el numero era:" + str(num))
+
+        if (flag == True):
+            opc = input("Desea volver a jugar: S/N")
+            if (opc== "S" or opc=="s"):
+                main(turnos+1)
+        intentos+=1
+
+
+
+
+
+main(1)
